21cf5b5f541af2d5cd2708a73c0aba7acb0df484
deepanshu-QM <cdeepanshu231@gmail.com>
2026-03-24 22:25:27 +0530

    DOM manipulation

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 2005
+const path = require("path");
 
 const notes = []; // Temp Place Where Our Notes will be stored
 app.use(express.json()) //middlewares
@@ -8,14 +9,20 @@ app.use(express.json()) //middlewares
 app.post("/notes",(req,res)=> {
     const note = req.body.note
     notes.push(note);
-    res.send({
+    res.json({
         message : "Task Done !"
     })
 })
 
+app.get("/notes" ,(req,res)=> {
+    res.json({
+        notes
+    })
+})
 
-
-
+app.get("/notes",(req,res)=> {
+    res.sendFile(path.join(__dirname, "frontend-html", "index.html"))
+})
 app.listen(PORT, ()=> {
     console.log(`Server Listen @ ${PORT}`)
 })
